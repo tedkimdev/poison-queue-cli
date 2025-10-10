119a4fa505cc5ca53b643de8671862dd77fa76cf
ted <dev.ted.kim@gmail.com>
2025-10-10 04:40:10 -0400

    feat: initial poison queue CLI setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+
+pub mod cli;
+use cli::*;
+
+#[tokio::main]
+async fn main() {
+    println!("Poison Queue CLI!");
+
+    let cli = cli::commands::Args::parse();
+
+    match cli.command {
+        Some(Commands::ListTopics) => {
+            todo!();
+        },
+        Some(Commands::ListMessages {
+            topic,
+        }) => {
+            todo!();
+        },
+        Some(Commands::ViewMessage {
+            message_id,
+        }) => {
+            todo!();
+        },
+        Some(Commands::EditMessage {
+            message_id,
+        }) => {
+            todo!();
+        },
+        Some(Commands::ArchiveMessage {
+            message_id,
+        }) => {
+            todo!();
+        },
+        Some(Commands::RepublishMessage {
+            message_id,
+        }) => {
+            todo!();
+        },
+        None => {
+            println!("Run with --help to see instructions");
+            std::process::exit(0);
+        }
+    }
+    
 }
